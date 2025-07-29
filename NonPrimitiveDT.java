5aa4a3a7e48af22329e94ee99dc9aa36e59e2d87
NarneAmruthaLekha <2420030477@klh.edu.in>
2025-07-29 10:59:02 +0530

    Add files via upload

---
+++ NonPrimitiveDT.java
@@ -0,0 +1,14 @@
+package practical_programs;
+
+public class NonPrimitiveDT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String greeting="Hello World!!";
+		System.out.println(greeting);
+
+
+	}
+
+}
